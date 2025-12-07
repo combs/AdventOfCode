384a4bea48d9f1cf4062ffc002588508194b88f4
Chris Combs <ccombs@gmail.com>
2025-12-07 11:54:27 -0500

    Day 7 part 2

--- 2025/07/07-2.py
+++ 2025/07/07-2.py
@@ -2,7 +2,7 @@ import sys
 import numpy
 
 temp = []
-SPLITTER = 10
+SPLITTER = -1
 BEAM = 1
 
 def replacer(thing):
@@ -25,24 +25,19 @@ with open(sys.argv[1], "r") as fh:
 
 matrix = numpy.array(temp, dtype=int)
 
-count = 0
 for row in range(matrix.shape[0]):
     if row==0:
         continue
+    for col, val in enumerate(matrix[row]): # Do a separate path to propagate beams
+        if matrix[row-1][col] >= BEAM and val == 0:
+            matrix[row][col] = matrix[row-1][col]
+
     for col, val in enumerate(matrix[row]):
-        if matrix[row-1][col] == BEAM and val == 0:
-            matrix[row][col] = BEAM
-
-        if val==SPLITTER and matrix[row-1][col] == BEAM: # A beam is above it
+        if val==SPLITTER and matrix[row-1][col] >= BEAM: # A beam is above it
             split = False
-            if col > 0 and matrix[row][col-1] != BEAM:
-                matrix[row][col-1] = BEAM
-                split = True
-            if col < (matrix.shape[1] - 1) and matrix[row][col+1] != BEAM:
-                matrix[row][col+1] = BEAM
-                split = True
-            if split:
-                count += 1
+            if col > 0:
+                matrix[row][col-1] += matrix[row-1][col]
+            if col < (matrix.shape[1] - 1):
+                matrix[row][col+1] += matrix[row-1][col]
 
-print(matrix)
-print(count)
+print(sum(list(matrix[-1,])))
